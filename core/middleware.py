8853610a4e1f0631bd84814dca28bfbf8139d6fd
SeafoamHare <130981975+SeafoamHare@users.noreply.github.com>
2023-04-25 22:31:08 +0800

    <feat> new Rate limiting header field

--- core/middleware.py
+++ core/middleware.py
@@ -42,10 +42,9 @@ def optimistic(cache,cache_key,last_refill_time):
 
 class OptimisticBucket:
     
-    def __init__(self, rate,):
+    def __init__(self, rate):
         self.cache = caches['default']
-        self.capacity, self.fill_rate = _split_rate(rate)
-        self.tokens = self.capacity
+        self.tokens, self.fill_rate = _split_rate(rate)
         self.last_update = time.time()
         self.key_prefix = 'OptimisticBucket'
     
@@ -62,19 +61,36 @@ class OptimisticBucket:
             adds = False
         
         cache_token = self.cache.get(cache_key,{'value':self.tokens,'last_refill_time':now})
+
+        #calculter new token
         if not adds:
             time_passed = now - cache_token['last_refill_time']
             if time_passed >= 0:
-                new_token = time_passed / self.fill_rate
+                new_token = self.tokens * time_passed / self.fill_rate
             else:
                 raise ImproperlyConfigured('time_passed must >= 0')
-        new_value = min(self.capacity, cache_token['value'] + new_token)
-        
+        new_value = min(self.tokens, cache_token['value'] + new_token)
 
         if tokens <= new_value:
             new_value = new_value - tokens
             if optimistic(self.cache,cache_key,cache_token['last_refill_time']):
-                self.cache.set(cache_key,{'value':new_value,'last_refill_time':now}, self.fill_rate)
+                try:
+                    self.cache.set(cache_key,{'value':new_value,'last_refill_time':now}, self.fill_rate)
+                except socket.gaierror:
+                    raise ImproperlyConfigured('set new value error')
+                return {
+                    'limit': self.tokens,
+                    'count': new_value,
+                    'time_left': (self.tokens - new_value) / self.tokens * self.fill_rate
+                    }
+        else:
+            return {
+                'Retry-At': self.tokens * (tokens - new_value) / self.fill_rate
+                }
+        
+
+    def is_ratelimit(self,headerfile):
+        if 'Retry-At' in headerfile:
             return True
         else:
             return False
@@ -83,10 +99,23 @@ class OptimisticBucket:
 class OptimisticBucketMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response        
-        self.token_bucket = OptimisticBucket(rate='1/s')
+        self.token_bucket = OptimisticBucket(rate='2/s')
+        self.headerfile = ""
 
     def __call__(self, request):
-        if not self.token_bucket.handle(request,1):
-            return HttpResponse('Rate limit exceeded', status=429)
+        
+        if callable(self.token_bucket.handle):
+            self.headerfile = self.token_bucket.handle(request,1)
+        else:
+            raise ImproperlyConfigured('Could not get return from token_bucket.handle')
+        
+        if self.token_bucket.is_ratelimit(self.headerfile):
+            response = HttpResponse('Rate limit exceeded', status=429)
+            response['Retry-At'] = self.headerfile['Retry-At']
+            return response
+        
         response = self.get_response(request)
+        response['X-RateLimit-Limit'] = self.headerfile['limit']
+        response['X-RateLimit-Remaining'] = self.headerfile['count']
+        response['X-RateLimit-Reset'] = self.headerfile['time_left']
         return response
